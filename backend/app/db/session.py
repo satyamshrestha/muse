4c49bb3ebfeface60bd6c26f4da695f95086ba72
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-09-20 18:27:10 +0900

    feat: add database session dependency

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,5 +1,7 @@
+from collections.abc import Generator
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
 from app.core.config import settings
 
@@ -11,3 +13,11 @@ SessionLocal = sessionmaker(
     autoflush=False,
     autocommit=False,
 )
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
