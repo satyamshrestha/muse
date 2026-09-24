993f55d44d12a18d888b28fed4530c050d216ddb
Satyam Shrestha <shresthasatyam7@gmail.com>
2026-09-24 16:00:06 +0900

    chore: stop tracking env files and add invalid credentials exception and login service

--- backend/app/exceptions/user_exceptions.py
+++ backend/app/exceptions/user_exceptions.py
@@ -6,3 +6,10 @@ class UserAlreadyExistsException(AppException):
             status_code=409,
             detail="User already exists!"
         )
+
+class InvalidCredentialsException(AppException):
+    def __init__(self):
+        super().__init__(
+            status_code=401,
+            detail="Invalid Credentials! Please try again!"
+        )
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,7 +1,10 @@
-from app.auth.hashing import hash_password
+from app.auth.hashing import hash_password, verify_password
 from app.models.user import User
 from app.repositories.user_repository import UserRepository
-from app.exceptions.user_exceptions import (UserAlreadyExistsException)
+from app.exceptions.user_exceptions import (
+    UserAlreadyExistsException,
+    InvalidCredentialsException
+)
 
 
 class UserService:
@@ -29,5 +32,16 @@ class UserService:
         )
 
         self.repository.db.commit()
+
+        return user
+
+    def login(
+        self,
+        email: str,
+        password: str
+    ):
+        user = self.repository.get_by_email(email)
+        if not user or not verify_password(password, user.password_hash):
+            raise InvalidCredentialsException()        
 
         return user
